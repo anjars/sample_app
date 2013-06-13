1dfeb6f6fb110d5f508dc1e930a2ba8eda1994d0
anjars <anjars@gmail.com>
2013-06-13 22:53:53 +0200

    Ytterligere tester, forandre størrelse på Gravatar

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,12 +32,12 @@ describe "UserPages" do
 			end
 		end
 
-		 describe "after submission" do
-        before { click_button submit }
+		describe "after submission" do
+			before { click_button submit }
 
-        it { should have_selector('title', text: 'Sign up') }
-        it { should have_content('error') }
-      end
+			it { should have_selector('title', text: 'Sign up') }
+			it { should have_content('error') }
+		end
 
 		describe "with valid information" do
 			before do
@@ -49,6 +49,14 @@ describe "UserPages" do
 
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by_email('user@example.com') }
+
+				it { should have_selector('title', text: user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end
 		end
 	end
