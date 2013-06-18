16f7262c4cd540e458ef33f5ad2074e1ae66710f
anjars <anjars@gmail.com>
2013-06-18 13:14:26 +0200

    Forenkle tester og forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
  end
 
  def create
- 	user = User.find_by_email(params[:session][:email].downcase)
- 	if user && user.authenticate(params[:session][:password])
+ 	user = User.find_by_email(params[:email])
+ 	if user && user.authenticate(params[:password])
+ 		session[:user] = user.id
  		sign_in user
  		redirect_to user
  	else
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
 
  def destroy
  	sign_out
- 	redirect_to root_url
+ 	redirect_to root_path
  end
 
 
